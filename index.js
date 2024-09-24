f60f77820f48ebca3dfe715864e3e0ec8bee9c18
Aanand-Patil <anandpatil6440@gmail.com>
2024-09-24 22:58:19 +0530

    fruit world

---
+++ index.js
@@ -0,0 +1,13 @@
+let mainHeading = document.getElementById('main-heading')
+mainHeading.innerText = 'Fruit World'
+mainHeading.style.color = 'orange'
+let header = document.getElementById("header")
+header.style.backgroundColor = 'green'
+header.style.borderBottomColor = 'orange'
+header.style.borderStyle = 'solid '
+let basketHeading = document.getElementById('basket-heading')
+basketHeading.style.color = 'green'
+let thanks = document.getElementById("thanks")
+let para = document.createElement("p")
+para.innerText = 'Please visit us again'
+thanks.appendChild(para)
