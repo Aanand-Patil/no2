6de9e616ebc9824c66f7de230dd1270df3d18d23
Aanand-Patil <anandpatil6440@gmail.com>
2024-09-24 23:42:48 +0530

    changed li element style to italic

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-let mainHeading = document.getElementById('main-heading')
-mainHeading.innerText = 'Fruit World'
-mainHeading.style.color = 'orange'
-let header = document.getElementById("header")
-header.style.backgroundColor = 'green'
-header.style.borderBottomColor = 'orange'
-header.style.borderStyle = 'solid '
-let basketHeading = document.getElementById('basket-heading')
-basketHeading.style.color = 'green'
-let thanks = document.getElementById("thanks")
-let para = document.createElement("p")
-para.innerText = 'Please visit us again'
-thanks.appendChild(para)
-let fruits = document.getElementsByClassName("fruit")
-
-for (i = 0; i < fruits.length; i++) {
-    fruits[i].style.fontWeight = "bold"
+let fruits = document.getElementsByTagName("li")
+fruits[4].style.color = "blue";
+for (let i = 0; i < fruits.length; i++) {
+    fruits[i].style.fontStyle = "italic";
 }
-fruits[2].style.backgroundColor = "yellow"
